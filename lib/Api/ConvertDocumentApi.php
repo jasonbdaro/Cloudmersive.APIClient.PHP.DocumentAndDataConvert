f94b556dfbaa2719e09ddaed16b1a5e5d742feae
Jason Daro <23290574+jasonbdaro@users.noreply.github.com>
2021-10-20 00:23:45 +0800

    fix Query::build

--- lib/Api/ConvertDocumentApi.php
+++ lib/Api/ConvertDocumentApi.php
@@ -3691,7 +3691,7 @@ class ConvertDocumentApi
             $headers
         );
 
-        $query = \GuzzleHttp\Psr7\Utils::build($queryParams);
+        $query = \GuzzleHttp\Psr7\Query::build($queryParams);
         return new Request(
             'POST',
             $this->config->getHost() . $resourcePath . ($query ? "?{$query}" : ''),
